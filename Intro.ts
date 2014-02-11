b08569b6d964ca0b4a2076cdb1eb8411260db90b
thebeebs <martinbeeby@hotmail.com>
2014-02-11 20:33:19 +0000

    Adding c property so that call dosen't match interface

--- Intro.ts
+++ Intro.ts
@@ -1,18 +1,16 @@
-// You can define you own types using interfaces
-// Thing describes and object literal with an a number property
-// and a b string property.
-// Even though the process function dosen't return a type
-// We have given the compiler enogh information to infer
-// that n at the bottom will be of type number.
+// You can make c optional by postfixing it with a question mark.
+// This means it's still there but it is not required.
+// If you do try and pass C it must be of type boolean
+// There for the param is optional however the tyoe is not.
 interface Thing {
 	a: number;
 	b: string;
-	c: boolean;
+	c?: boolean;
 }
 
 function process(x: Thing){
 	return x.a;
 }
 
-var n = process({a: 10, b:'Beeby'});
+var n = process({a: 10, b:'Beeby', c:'fail'});
 n.toFixed(0.0);
