661668766a3db173495fd53c2d6fdf33ff6059c9
thebeebs <martinbeeby@hotmail.com>
2014-02-11 19:55:23 +0000

    Add an interface

--- Intro.ts
+++ Intro.ts
@@ -1,7 +1,12 @@
 // You can state that x is a fucntion
 // You can describe the shape of an object
 // so in this instance a will have a string propert called
-// a and a b property of type number.
-function process(x: {a: string; b: number}){
-	return x.a.length;
+// a and a b property of type number
+interface Thing {
+	a: number;
+	b: string;
+}
+
+function process(x: Thing){
+	return x.b.length;
 }
