977d834ab654ac389aaa9dab9979ca5382c93f82
thebeebs <martinbeeby@hotmail.com>
2014-02-11 20:50:49 +0000

    Give an optional to the function property

--- Intro.ts
+++ Intro.ts
@@ -1,13 +1,10 @@
-// You can make c optional by postfixing it with a question mark.
-// This means it's still there but it is not required.
-// If you do try and pass C it must be of type boolean
-// There for the param is optional however the tyoe is not.
+// Functions can have optional parameters too
 interface Thing {
 	a: number;
 	b: string;
-	foo(x: string, n: number): string;
+	foo(x: string, n?: number): string;
 }
 
 function process(x: Thing){
-	return x.foo("string",10);
+	return x.foo("string");
 }
