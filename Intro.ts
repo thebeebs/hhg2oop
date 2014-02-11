480f2b50f674983776a6c99c582a02ce99bb5ef5
thebeebs <martinbeeby@hotmail.com>
2014-02-11 15:45:31 +0000

    Update Intro File with Bool

--- Intro.ts
+++ Intro.ts
@@ -1,7 +1,6 @@
 // So If I change the x to a number. Then
 // the product of x+x will be a number. 
 // So v is inferred to be a number
-function process(x : number){
+function process(x: bool){
 	var v = x + x;
-	alert(v);
 }
