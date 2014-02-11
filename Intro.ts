d7937e7b1d2b70b66237976b2103925e0eaccbd8
thebeebs <martinbeeby@hotmail.com>
2014-02-11 19:20:16 +0000

    Update add string array

--- Intro.ts
+++ Intro.ts
@@ -1,6 +1,7 @@
 // You can state that x is a fucntion
-// That returns a string.
-// If you call x the result will be a string.
-function process(x: () => string){
-	x().toLowerCase();
+// You can describe the shape of an object
+// so in this instance a will have a string propert called
+// a and a b property of type number.
+function process(x: {a: string; b: number}){
+	return x.a.length;
 }
