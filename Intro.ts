85b110236a741350615a8175839b030c29db0b29
thebeebs <martinbeeby@hotmail.com>
2014-02-11 14:29:47 +0000

    Added example 2

--- Intro.ts
+++ Intro.ts
@@ -1,8 +1,7 @@
-// An ordinary function in JavaScript.
-// This look odd. We are putting a property on x
-// and then trying to add it. TypeScript in this
-// instance will assume that 
-function(x){
+// If I annotate xas a string things start to light up
+// In sublime you will nottice errors being thrown
+// Here it is saying that a string can't have a name property
+function(x : string){
 	x.name = "foo";
 	var v = x + x;
 	alert(v);
