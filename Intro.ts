c963106d25bef80b78fec947e66e678e0437d71b
thebeebs <martinbeeby@hotmail.com>
2014-02-11 15:25:31 +0000

    Adding number Example

--- Intro.ts
+++ Intro.ts
@@ -1,8 +1,7 @@
-// If I annotate xas a string things start to light up
-// In sublime you will nottice errors being thrown
-// Here it is saying that a string can't have a name property
-function(x : string){
-	x.name = "foo";
+// So If I change the x to a number. Then
+// the product of x+x will be a number. 
+// So v is inferred to be a number
+function process(x : number){
 	var v = x + x;
 	alert(v);
 }
