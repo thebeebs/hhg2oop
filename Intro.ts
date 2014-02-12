dafe8c1b544ae024dd3cab035ecd225f2eaec098
thebeebs <martinbeeby@hotmail.com>
2014-02-12 11:04:12 +0000

    TypeScript models the entire JavaScript Dom

--- Intro.ts
+++ Intro.ts
@@ -1,24 +1,2 @@
-// You can add an interface to an object
-// So the miss spelling of clear() is picked up and the app will not compile.
-interface Rapper {
-	say(x: string): void;
-	clear(): void;
-	speak(): string;
-}
-
-function makeRapper(): Rapper{
-	var words;
-	return {
-		say: function (x: string){ 
-			words = x;
-			},
-		clearSpeltWrong: function (){ 
-			words = '';
-			},
-		speak: function(){
-			return words;
-		}
-	};
-}
-
-makeRapper();
+// Infered information from declartion 8,000 lines.
+window.onmousemove = function(e) {e.clientX};
